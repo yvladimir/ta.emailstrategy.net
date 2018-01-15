51ce90b6774d1a7c496d593fe1a4ad973db268b3
Yaroshenko Vladimir <yaroshenko.vladimir@gmail.com>
2018-01-15 19:12:01 +0200

    Remove login page from frontend

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -39,18 +39,6 @@ AppAsset::register($this);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
     ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => $menuItems,
